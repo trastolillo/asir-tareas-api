ed3938e4734edba5fcba68ec5976e24e251df8dd
trastolillo <trastolillo@tutanota.com>
2021-09-17 16:31:36 +0200

    variables de entorno

--- routes.go
+++ routes.go
@@ -85,24 +85,24 @@ func setupRoutesForTareas(router *mux.Router) {
 		}).Methods(http.MethodDelete)
 }
 
-func enableCORS(router *mux.Router) {
-	router.PathPrefix("/").HandlerFunc(
-		func(w http.ResponseWriter, req *http.Request) {
-			w.Header().Set("Acces-Control-Allow-Origin", AllowedCORSDomain)
-		}).Methods(http.MethodOptions)
-	router.Use(middlewareCors)
-}
+// func enableCORS(router *mux.Router) {
+// 	router.PathPrefix("/").HandlerFunc(
+// 		func(w http.ResponseWriter, req *http.Request) {
+// 			w.Header().Set("Acces-Control-Allow-Origin", AllowedCORSDomain)
+// 		}).Methods(http.MethodOptions)
+// 	router.Use(middlewareCors)
+// }
 
-func middlewareCors(next http.Handler) http.Handler {
-	return http.HandlerFunc(
-		func(w http.ResponseWriter, req *http.Request) {
-			w.Header().Set("Access-Control-Allow-Origin", AllowedCORSDomain)
-			w.Header().Set("Access-Control-Allow-Credentials", "true")
-			w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
-			w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
-			next.ServeHTTP(w, req)
-		})
-}
+// func middlewareCors(next http.Handler) http.Handler {
+// 	return http.HandlerFunc(
+// 		func(w http.ResponseWriter, req *http.Request) {
+// 			w.Header().Set("Access-Control-Allow-Origin", AllowedCORSDomain)
+// 			w.Header().Set("Access-Control-Allow-Credentials", "true")
+// 			w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+// 			w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+// 			next.ServeHTTP(w, req)
+// 		})
+// }
 
 func respondWithSuccess(data interface{}, w http.ResponseWriter) {
 	w.WriteHeader(http.StatusOK)
--- vars.go
+++ vars.go
@@ -3,20 +3,18 @@ package main
 import (
 	"fmt"
 	"os"
-
-	"github.com/joho/godotenv"
 )
 
-var _ = godotenv.Load(".env")
+// var _ = godotenv.Load(".env")
 
 var (
 	ConnectionString = fmt.Sprintf("%s:%s@tcp(%s:%s)/%s",
-		os.Getenv("user"),
-		os.Getenv("pass"),
+		os.Getenv("userBD"),
+		os.Getenv("passBD"),
 		os.Getenv("host"),
-		os.Getenv("port"),
-		os.Getenv("db_name"),
+		os.Getenv("PORT"),
+		os.Getenv("nameBD"),
 	)
 )
 
-const AllowedCORSDomain = "http://localhost"
+// const AllowedCORSDomain = "http://localhost"
