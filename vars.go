9306a44272ca0d506be109b1f4c1af5fb14e66f8
trastolillo <trastolillo@tutanota.com>
2021-09-18 02:12:19 +0200

    errores

--- vars.go
+++ vars.go
@@ -11,7 +11,7 @@ var (
 	ConnectionString = fmt.Sprintf("%s:%s@tcp(%s:%s)/%s",
 		os.Getenv("userBD"),
 		os.Getenv("passBD"),
-		os.Getenv("host"),
+		os.Getenv("hostBD"),
 		os.Getenv("PORT"),
 		os.Getenv("nameBD"),
 	)
