ba29ad01e8e7be06a0ea974aa3297715afe71df3
trastolillo <trastolillo@tutanota.com>
2021-09-18 02:03:03 +0200

    errores

--- vars.go
+++ vars.go
@@ -8,13 +8,15 @@ import (
 // var _ = godotenv.Load(".env")
 
 var (
-	ConnectionString = fmt.Sprintf("%s:%s@tcp(%s:%s)/%s",
+	ConnectionString = fmt.Sprintf("%s:%s@tcp(%s:3306)/%s",
 		os.Getenv("userBD"),
 		os.Getenv("passBD"),
 		os.Getenv("host"),
-		os.Getenv("PORT"),
+		// os.Getenv("PORT"),
 		os.Getenv("nameBD"),
 	)
 )
+
+// var ConnectionString = "beac0a503e9041:6b0c0bfd@tcp"
 
 // const AllowedCORSDomain = "http://localhost"
